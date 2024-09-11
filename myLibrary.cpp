2a7e0a1568254ca8b3a03ce22c817e07ae9a81fd
Meg Hsu <mhsu@sfsu.edu>
2024-09-11 15:56:05 -0700

    Changeed code to account for edge cases (vector size, invalid input).

--- myLibrary.cpp
+++ myLibrary.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctype.h>
 using namespace std;
 
 vector<int> removeDuplicates(vector<int>);
@@ -9,26 +10,29 @@ vector<int> removeDuplicates(vector<int> numberList){
     int vectorsize = numberList.size();
     if (vectorsize < 2) return numberList;
     vector<int> copyList;
-    bool found = false;
+    bool found;
 
     for (int i = 0; i < vectorsize; i++){ // iterate through input
-        for (int j = 0; j < copyList.size(); j++){ // iterate through copy of input
-            if (numberList[i] == copyList[j]) {
-                found = true;
-                continue;
+        found = false;
+        if (numberList[i] > -1){
+            for (int j = 0; j < copyList.size(); j++){ // iterate through copy of input
+                if (numberList[i] == copyList[j]) {
+                    found = true;
+                    continue;
+                }
+            }
+            if (!found){
+                copyList.push_back(numberList[i]);
+                cout << numberList[i] << endl;
             }
         }
-        if (!found){
-            copyList.push_back(numberList[i]);
-        }
-        found = false;
     }
-
     return copyList;
 }
 
 pair<int, int> sumIndices(vector<int>numberList, int sum){
     pair<int, int> indices = {-1, -1};
+    if (sum < 1) return indices;
     int expected;
     int vectorsize = numberList.size();
     if (vectorsize < 2){

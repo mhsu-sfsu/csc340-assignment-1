2fa113738b58cec14bece6e307c4333121dd4371
Meg Hsu <mhsu@sfsu.edu>
2024-09-12 15:47:41 -0700

    Revert "Removed accidentally pushed files"
    This reverts commit a21a37ae6b5fac168b111b3d0f40ed8c015e551c.

--- Meg_Hsu_asmt1/myLibrary.h
+++
@@ -1,5 +0,0 @@
-#ifndef MYLIBRARY_H
-#define MYLIBRARY_H
-    vector<int> removeDuplicates(vector<int>);
-    pair<int, int> sumIndices(vector<int>, int);
-#endif
--- myLibrary.cpp
+++ myLibrary.cpp
@@ -4,48 +4,45 @@
 using namespace std;
 
 vector<int> removeDuplicates(vector<int>);
-pair<int, int> sumIndices(vector<int>, int);
+vector<int> sumIndicies(vector<int>, int);
 
-//removeDuplicates takes in a vector of numbers called numberList and returns a new vector containing positive nonduplicate numbers
 vector<int> removeDuplicates(vector<int> numberList){
-
     int vectorsize = numberList.size();
     if (vectorsize < 2) return numberList;
     vector<int> copyList;
     bool found;
 
-    for (int i = 0; i < vectorsize; i++){ // Iterate through input
+    for (int i = 0; i < vectorsize; i++){ // iterate through input
         found = false;
-        if (numberList[i] > -1){ // Disregards any negative input 
-            for (int j = 0; j < copyList.size(); j++){ 
-                if (numberList[i] == copyList[j]) { // If the value is found, change boolean and continue
+        if (numberList[i] > -1){
+            for (int j = 0; j < copyList.size(); j++){ // iterate through copy of input
+                if (numberList[i] == copyList[j]) {
                     found = true;
                     continue;
                 }
             }
-            if (!found){ //After going through the values in the vector being returned, if there is no match, adds it to the vector
+            if (!found){
                 copyList.push_back(numberList[i]);
+                cout << numberList[i] << endl;
             }
         }
     }
     return copyList;
 }
 
-//sumIndices is a function which takes a vector of nonrepeating numbers and an integer sum. It returns a pair containing the two indices of the vector which have values adding up to the sum. Otherwise, returns a pair containing the numbers -1, -1.
 pair<int, int> sumIndices(vector<int>numberList, int sum){
     pair<int, int> indices = {-1, -1};
-
-    // If the sum or vector size are invalid, immediately returns default values
     if (sum < 1) return indices;
+    int expected;
     int vectorsize = numberList.size();
     if (vectorsize < 2){
+        indices.first = -1;
+        indices.second = -1;
         return indices;
     }
-
-    int expected; // This number is the number being searched for in the following loop
     for (int i = 0; i < vectorsize; i++){
         expected = sum - numberList[i];
-        for (int j = (i+1); j < vectorsize; j++){ //Loops through remaining vector to search for matches, hence j being set to (i + 1).
+        for (int j = (i+1); j < vectorsize; j++){
             if (numberList[j] == expected){
                 indices.first = i;
                 indices.second = j;
@@ -53,5 +50,7 @@ pair<int, int> sumIndices(vector<int>numberList, int sum){
             }
         }
     }
+    indices.first = -1;
+    indices.second = -1;
     return indices;
 }
